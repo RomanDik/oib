7e452311630077c0148188f81a10e2564062603d
Roman Dik <roman75d@yandex.ru>
2024-05-29 19:40:12 +0400

    2_laba_generators

---
+++ lab_2/generator.java
@@ -0,0 +1,16 @@
+package lab_2;
+
+import java.util.Random;
+
+public class generator {
+    private static final int SIZE = 128;
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        for (int i = 0; i < SIZE; i++) {
+            int bit = random.nextInt(2);
+            System.out.print(bit);
+        }
+    }
+}
